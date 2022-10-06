462deeaeef638144ae308a12c59f433fb93ea812
scarfada <83604371+scarfada@users.noreply.github.com>
2022-10-06 15:32:35 -0300

    1 binaries uploaded

---
+++ public/imagem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Imagem =()=> {
+  return(
+    <div>
+      <img src ="http://localhost:3000/details/img/myImage.png" />
+    </div>
+  )
+}
+
+export default Imagem
--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './style.css';
 
+
 export default function App() {
-  return <h3>Avançando em React</h3>;
+  return(
+ <div> 
+    <h3>Avançando em React</h3>
+    
+ </div>        
+)  
 }
