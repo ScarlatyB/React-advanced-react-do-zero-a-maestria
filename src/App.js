3de404cf5a735fac3ef72cdbb3196a92c6182ad0
scarfada <83604371+scarfada@users.noreply.github.com>
2022-10-06 18:51:36 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './style.css';
-
+// import Imagem from './public/morango.jpg';
 
 export default function App() {
-  return(
- <div> 
-    <h3>Avançando em React</h3>
-    
- </div>        
-)  
+  return (
+    <div>
+      <h3>Avançando em React</h3>
+    </div>
+  );
 }
